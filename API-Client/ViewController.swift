6ec77170513a12f5f30b404609a7859dd36a71e9
Victor Adu <aduvictor@gmail.com>
2017-04-28 01:04:10 -0700

    Initial Commit

---
+++ API-Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  API-Client
+//
+//  Created by Victor  Adu on 4/28/17.
+//  Copyright © 2017 Victor  Adu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
